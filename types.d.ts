da71ca4e996f642aad047a0dd826604abf942186
Francisco Di Nucci <109241728+pacodinucci@users.noreply.github.com>
2026-02-20 09:17:21 -0300

    refactor: reuse backend model types in global declarations

--- types.d.ts
+++ types.d.ts
@@ -10,9 +10,15 @@ type StaticData = {
   totalMemoryGB: number;
 };
 
+type BackendTruthRegistry = import("./src/electron/backend/models.js").BackendTruthRegistry;
+type BackendBootstrapStatus = import("./src/electron/backend/models.js").BackendBootstrapStatus;
+
 type EventPayloadMapping = {
   statistics: Statistics;
   getStaticData: StaticData;
+  backendGetTruthRegistry: BackendTruthRegistry;
+  backendSeedInitialData: BackendBootstrapStatus;
+  backendGetBootstrapStatus: BackendBootstrapStatus;
 };
 
 type UnsuscribeFunction = () => void;
@@ -23,5 +29,8 @@ interface Window {
       callback: (statistics: Statistics) => void
     ) => UnsuscribeFunction;
     getStaticData: () => Promise<StaticData>;
+    backendGetTruthRegistry: () => Promise<BackendTruthRegistry>;
+    backendSeedInitialData: () => Promise<BackendBootstrapStatus>;
+    backendGetBootstrapStatus: () => Promise<BackendBootstrapStatus>;
   };
 }
